a766385da804b226e21fa5643d8ed4914da44988
EdwinOrtegaGutierrez <eddyded49@gmail.com>
2024-09-28 11:20:07 -0600

    Feat: se agregaron componentes nuevos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tallerAngular';
+  
+  verPerfil(){
+    alert('Ver Perfil');
+  }
+
+  conocerMas(){
+    alert('Conocer Mas');
+  }
+
+
 }
